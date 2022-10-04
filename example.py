6f72ccc57fb4ebcccfd49becc7d285175a2e36f8
Andrew Marsh <marshandrewg@gmail.com>
2022-10-04 14:22:43 -0400

    Adding test docs and working example

--- example.py
+++ example.py
@@ -1,111 +1,66 @@
-from flask import Flask, request, make_response, Response
 import os
-import json
+# Use the package we installed
+from slack_bolt import App
 
-from slackclient import SlackClient
-
-# Your app's Slack bot user token
-SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
-SLACK_VERIFICATION_TOKEN = os.environ["SLACK_VERIFICATION_TOKEN"]
-
-# Slack client for Web API requests
-slack_client = SlackClient(SLACK_BOT_TOKEN)
-
-# Flask webserver for incoming traffic from Slack
-app = Flask(__name__)
-
-# Helper for verifying that requests came from Slack
-def verify_slack_token(request_token):
-    if SLACK_VERIFICATION_TOKEN != request_token:
-        print("Error: invalid verification token!")
-        print("Received {} but was expecting {}".format(request_token, SLACK_VERIFICATION_TOKEN))
-        return make_response("Request contains invalid Slack verification token", 403)
-
-
-# The endpoint Slack will load your menu options from
-@app.route("/slack/message_options", methods=["POST"])
-def message_options():
-    # Parse the request payload
-    form_json = json.loads(request.form["payload"])
-
-    # Verify that the request came from Slack
-    verify_slack_token(form_json["token"])
-
-    # Dictionary of menu options which will be sent as JSON
-    menu_options = {
-        "options": [
-            {
-                "text": "Cappuccino",
-                "value": "cappuccino"
-            },
-            {
-                "text": "Latte",
-                "value": "latte"
-            }
-        ]
-    }
-
-    # Load options dict as JSON and respond to Slack
-    return Response(json.dumps(menu_options), mimetype='application/json')
-
-
-# The endpoint Slack will send the user's menu selection to
-@app.route("/slack/message_actions", methods=["POST"])
-def message_actions():
-
-    # Parse the request payload
-    form_json = json.loads(request.form["payload"])
-
-    # Verify that the request came from Slack
-    verify_slack_token(form_json["token"])
-
-    # Check to see what the user's selection was and update the message accordingly
-    selection = form_json["actions"][0]["selected_options"][0]["value"]
-
-    if selection == "cappuccino":
-        message_text = "cappuccino"
-    else:
-        message_text = "latte"
-
-    response = slack_client.api_call(
-      "chat.update",
-      channel=form_json["channel"]["id"],
-      ts=form_json["message_ts"],
-      text="One {}, right coming up! :coffee:".format(message_text),
-      attachments=[] # empty `attachments` to clear the existing massage attachments
-    )
-
-    # Send an HTTP 200 response with empty body so Slack knows we're done here
-    return make_response("", 200)
-
-# Send a Slack message on load. This needs to be _before_ the Flask server is started
-
-# A Dictionary of message attachment options
-attachments_json = [
-    {
-        "fallback": "Upgrade your Slack client to use messages like these.",
-        "color": "#3AA3E3",
-        "attachment_type": "default",
-        "callback_id": "menu_options_2319",
-        "actions": [
-            {
-                "name": "bev_list",
-                "text": "Pick a beverage...",
-                "type": "select",
-                "data_source": "external"
-            }
-        ]
-    }
-]
-
-# Send a message with the above attachment, asking the user if they want coffee
-slack_client.api_call(
-  "chat.postMessage",
-  channel="#python",
-  text="Would you like some coffee? :coffee:",
-  attachments=attachments_json
+# Initializes your app with your bot token and signing secret
+app = App(
+    token=os.environ.get("SLACK_BOT_TOKEN"),
+    signing_secret=os.environ.get("SLACK_SIGNING_SECRET")
 )
 
-# Start the Flask server
+# Add functionality here
+# @app.event("app_home_opened") etc
+@app.event("app_home_opened")
+def update_home_tab(client, event, logger):
+  try:
+    # views.publish is the method that your app uses to push a view to the Home tab
+    client.views_publish(
+      # the user that opened your app's app home
+      user_id=event["user"],
+      # the view object that appears in the app home
+      view={
+        "type": "home",
+        "callback_id": "home_view",
+
+        # body of the view
+        "blocks": [
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "*Welcome to your _App's Home_* :tada:"
+            }
+          },
+          {
+            "type": "divider"
+          },
+          {
+            "type": "section",
+            "text": {
+              "type": "mrkdwn",
+              "text": "This button won't do much for now but you can set up a listener for it using the `actions()` method and passing its unique `action_id`. See an example in the `examples` folder within your Bolt app."
+            }
+          },
+          {
+            "type": "actions",
+            "elements": [
+              {
+                "type": "button",
+                "text": {
+                  "type": "plain_text",
+                  "text": "Click me!"
+                }
+              }
+            ]
+          }
+        ]
+      }
+    )
+  
+  except Exception as e:
+    logger.error(f"Error publishing home tab: {e}")
+
+# Start your app
 if __name__ == "__main__":
-    app.run()
+    app.start(port=int(os.environ.get("PORT", 3000)))
+
