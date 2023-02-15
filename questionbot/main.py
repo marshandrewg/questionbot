d6e832caf424907442ef44d5899b5c5004664fa8
Andrew Marsh <marshandrewg@gmail.com>
2023-02-15 10:29:56 -0500

    Adding error handling to hint missing fields

--- questionbot/main.py
+++ questionbot/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 from model.Question import Question
 from model.Questionnaire import Questionnaire
 
+import logging
+from fastapi import FastAPI, Request, status
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+
 app = FastAPI()
 
 # Think about how you would preserve async for image generation
@@ -19,3 +24,10 @@ async def read_questionnaire(questionnaire_id: int):
 @app.post("/questionnaires/")
 async def create_questionnaire(questionnaire: Questionnaire):
     return questionnaire
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+	exc_str = f'{exc}'.replace('\n', ' ').replace('   ', ' ')
+	logging.error(f"{request}: {exc_str}")
+	content = {'status_code': 10422, 'message': exc_str, 'data': None}
+	return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
